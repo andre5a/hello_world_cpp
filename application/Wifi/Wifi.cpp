d37162e62c379b2d7b732acd2d373f32e26ec354
Andre Quinta <andrequinta@gmail.com>
2022-01-02 18:17:11 +0000

    Mutex

--- application/Wifi/Wifi.cpp
+++ application/Wifi/Wifi.cpp
@@ -3,8 +3,47 @@
 namespace WIFI
 {
     char Wifi::mac_addr_cstr[]{};
-    std::mutex Wifi::first_call_mutx;
-    bool Wifi::first_call{false};
+    SemaphoreHandle_t Wifi::first_call_mutx{nullptr};
+    StaticSemaphore_t Wifi::first_call_mutx_buffer{};
+    bool Wifi::first_call{true};
+
+    Wifi::Wifi(void)
+    {
+
+        // std::lock_guard<std::mutex> guard(first_call_mutx);
+        if (!first_call_mutx)
+        {
+            first_call_mutx = xSemaphoreCreateRecursiveMutexStatic(&first_call_mutx_buffer);
+            configASSERT(first_call_mutx);
+            configASSERT(pdPASS == xSemaphoreGive(first_call_mutx));
+        }
+        // configASSERT(first_call_mutx);
+        bool it_worked = false;
+        for (int i = 0; i < 3; ++i)
+        {
+            if (pdPASS == xSemaphoreTake(first_call_mutx, pdSECOND))
+            {
+                if (true == first_call)
+                {
+                    if (ESP_OK != _get_mac())
+                        esp_restart();
+                    first_call = false;
+                }
+                xSemaphoreGive(first_call_mutx);
+                it_worked = true;
+                break;
+            }
+            else
+            {
+                ESP_LOGI("WIFI", "Failed to get mutex (attempt %u)", i + 1);
+                continue;
+            }
+        }
+        if (!it_worked)
+        {
+            esp_restart();
+        }
+    }
     esp_err_t Wifi::_get_mac(void)
     {
         uint8_t mac_byte_buffer[6]{};
